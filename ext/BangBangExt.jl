f4881cdae72955b29128f5d0e85f59ba3c6b41f9
matthias314 <matthias314@posteo.net>
2024-11-05 18:40:24 -0500

    updated `BangBang` support

--- ext/BangBangExt.jl
+++ ext/BangBangExt.jl
@@ -4,18 +4,19 @@ using SmallCollections
 
 using BangBang: BangBang, NoBang, Mutator
 
-BangBang.implements(::Mutator, ::Type{<:SmallBitSet}) = false
+BangBang.implements(::Mutator, ::Type{<:Union{SmallDict, SmallSet, SmallBitSet}}) = false
 
 for f in (:push, :pop, :delete)
-    @eval NoBang.$f(v::SmallBitSet, args...) = $f(v, args...)
+    @eval NoBang.$f(v::SmallDict, x::Pair) = $f(v, x)
+    @eval NoBang.$f(v::Union{SmallSet, SmallBitSet}, x) = $f(v, x)
 end
 
-const CapacityVector = Union{SmallVector,PackedVector}
+const CapacityVector = Union{SmallVector, PackedVector}
 
 BangBang.implements(::Mutator, ::Type{<:CapacityVector}) = false
 
 for f in (:push, :pop, :pushfirst, :popfirst, :deleteat, :append)
-    @eval NoBang.$f(v::CapacityVector, args...) = $f(v, args...)
+    @eval NoBang.$f(v::CapacityVector, x) = $f(v, x)
 end
 
 BangBang.NoBang._setindex(v::CapacityVector, args...) = Base.setindex(v, args...)
